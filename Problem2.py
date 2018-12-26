949c4ff5e4f4989e8e0ad6240e92d51e59c25ea5
Roman Wang <romanwang@Romans-MBP.fios-router.home>
2018-12-25 22:22:31 -0500

    problem 2

--- Problem2.py
+++ Problem2.py
@@ -1,6 +1,8 @@
 __author__ = "Roman Wang"
-import scipy.constants as constants
+
 import math
+import scipy.constants as constants
+from decimal import *
 
 # Fibonacci as sum of two geometric series - can be solved for by solving associated homogenous recurrence relation
 def fib(n):
@@ -14,8 +16,14 @@ def fib(n):
 # Notice all even fibonacci numbers are 3 apart from the properties of adding odd/even numbers
 # We can produce a solution that is constant instead of O(n) and (n^2) by using the solved fib equation and algebraic manipulation
 def fib_even(n):
-    pass
+    phi = constants.golden
+    psi = 1-phi
+
+    geom_phi = phi**3 * (1-phi**(3*n))/(1-phi**3)
+    geom_psi = psi**3 * (1-psi**(3*n))/(1-psi**3)
+    return round((geom_phi-geom_psi)/(math.sqrt(5)))
 
 if __name__ == "__main__":
     print(constants.golden)
-    print(fib(6))
+    print(fib(33), fib(34))
+    print(fib_even(33//3))
